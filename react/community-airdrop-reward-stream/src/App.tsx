f2a0ffb371bf22e2ea0fabaefe342f0c72c99525
aramalipoor <aram.alipoor@gmail.com>
2022-06-17 21:41:08 +0200

    feat: update sdk to latest version

--- react/community-airdrop-reward-stream/src/App.tsx
+++ react/community-airdrop-reward-stream/src/App.tsx
@@ -1,8 +1,4 @@
-import {
-  RequireChain,
-  RequireConnect,
-  VestedHolderStreamClaimingSection,
-} from "flair-sdk";
+import { VestedHolderStreamClaimingSection } from "flair-sdk";
 
 const STREAM_CHAIN_ID = "137";
 const STREAM_CONTRACT_ADDRESS = "0x297b26231c5e06d807a6a33835618c5bc6e8fffb";
@@ -10,14 +6,14 @@ const STREAM_CONTRACT_ADDRESS = "0x297b26231c5e06d807a6a33835618c5bc6e8fffb";
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <RequireConnect>
-        <RequireChain requiredChainId={Number(STREAM_CHAIN_ID)}>
-          <VestedHolderStreamClaimingSection
-            chainId={Number(STREAM_CHAIN_ID)}
-            contractAddress={STREAM_CONTRACT_ADDRESS}
-          />
-        </RequireChain>
-      </RequireConnect>
+      {/* Import the whole prebuilt section component */}
+      <VestedHolderStreamClaimingSection
+        chainId={Number(STREAM_CHAIN_ID)}
+        contractAddress={STREAM_CONTRACT_ADDRESS}
+      />
+
+      {/* OR, if you want control over each component (supply counter, mint button, etc) */}
+      {/* you can copy the source code from https://github.com/0xflair/typescript-sdk/blob/main/packages/react-token-streams/src/presets/vested-holder-preset/sections/VestedHolderStreamClaimingSection.tsx#L28-L102 */}
     </div>
   );
 }
--- react/custom-nft-minting-sales/src/App.tsx
+++ react/custom-nft-minting-sales/src/App.tsx
@@ -1,23 +1,20 @@
-import {
-  RequireChain,
-  RequireConnect,
-  CollectionSalesMintingSection,
-} from "flair-sdk";
+import { CollectionSalesMintingSection } from "flair-sdk";
 
 const COLLECTION_CHAIN_ID = "137";
-const COLLECTION_CONTRACT_ADDRESS = "0x564b577b62133b53ad2774711e40e53a6362b553";
+const COLLECTION_CONTRACT_ADDRESS =
+  "0x564b577b62133b53ad2774711e40e53a6362b553";
 
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <RequireConnect>
-        <RequireChain requiredChainId={Number(COLLECTION_CHAIN_ID)}>
-          <CollectionSalesMintingSection
-            chainId={Number(COLLECTION_CHAIN_ID)}
-            contractAddress={COLLECTION_CONTRACT_ADDRESS}
-          />
-        </RequireChain>
-      </RequireConnect>
+      {/* Import the whole prebuilt section component */}
+      <CollectionSalesMintingSection
+        chainId={Number(COLLECTION_CHAIN_ID)}
+        contractAddress={COLLECTION_CONTRACT_ADDRESS}
+      />
+
+      {/* OR, if you want control over each component (supply counter, mint button, etc) */}
+      {/* you can copy the source code from https://github.com/0xflair/typescript-sdk/blob/main/packages/react-nft-collections/src/extensions/sales/sections/CollectionSalesMintingSection.tsx#L30-L154 */}
     </div>
   );
 }
