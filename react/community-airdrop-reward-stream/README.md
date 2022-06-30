d269310724e91bf4ccbd62e716de14641da1bb99
aramalipoor <aram.alipoor@gmail.com>
2022-06-30 17:11:54 +0200

    feat: update the community reward based on the new sdk

--- react/community-airdrop-reward-stream/README.md
+++ react/community-airdrop-reward-stream/README.md
@@ -10,10 +10,14 @@ This example React app uses Flair token streams to render a claiming widget for 
 ## :fire: Quick Start
 
 1. Create a new stream in [Flair's dashboard](https://app.flair.finance/streams). Note that you will be the full owner of staking contract.
-2. Grab your contract address and chain ID, and update [App.tsx](./src/App.tsx):
+2. Make sure to create "Reward for holding NFTs"
+
+![Screenshot](./preset-selection.png)
+
+3. Grab your contract address and chain ID, and update [App.tsx](./src/App.tsx):
    * Set `STREAM_CONTRACT_ADDRESS` to your deployed contract address you get from Flair's dashboard > Streams > your-stream > Deploy tab.
    * Set `STREAM_CHAIN_ID` depending on the contract chain. Use `1` for Eth mainnet, `4` for Rinkeby testnet, `137` for Polygon mainnet.
-3. Clone the repo, install dependencies and run the react app in the `community-airdrop-reward-stream` directory:
+4. Clone the repo, install dependencies and run the react app in the `community-airdrop-reward-stream` directory:
 
    ```sh
    git clone https://github.com/0xflair/examples
@@ -25,7 +29,7 @@ This example React app uses Flair token streams to render a claiming widget for 
    npm start
    ```
 
-4. Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+5. Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 ![Screenshot](./staking-screenshot.png)
 
--- react/community-airdrop-reward-stream/src/App.tsx
+++ react/community-airdrop-reward-stream/src/App.tsx
@@ -1,153 +1,134 @@
 import {
+  Environment,
   ConnectButton,
-  DisconnectButton,
   StreamClaimableAmount,
   StreamClaimButton,
   StreamClaimingProvider,
   StreamEligibleNfts,
+  StreamEmissionRate,
+  StreamEmissionTimeUnit,
   StreamStatusBar,
   StreamTotalClaimed,
   SwitchChainButton,
-  VestedHolderStreamClaimingProvider,
-  VestedHolderStreamRate,
-  VestedHolderStreamReleasedAmount,
-  VestedHolderStreamTimeUnit,
+  WalletDropdown,
 } from "flair-sdk";
 
 const STREAM_CHAIN_ID = "137";
-const STREAM_CONTRACT_ADDRESS = "0x297b26231c5e06d807a6a33835618c5bc6e8fffb";
+const STREAM_CONTRACT_ADDRESS = "0xff928c667452358a7f78f90b7b618d56cfc69eef";
 
 function App() {
+  const chainId = Number(STREAM_CHAIN_ID);
+  const contractAddress = STREAM_CONTRACT_ADDRESS;
+
   const buttonClass =
     "mt-4 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed";
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <StreamClaimingProvider
-        chainId={Number(STREAM_CHAIN_ID)}
-        contractAddress={STREAM_CONTRACT_ADDRESS}
-      >
-        <VestedHolderStreamClaimingProvider>
-          {({ data: { stream } }) => (
-            <>
-              <style type="text/css">{`html {background: none transparent !important;}`}</style>
-              <main className="h-fit mx-auto max-w-lg flex items-center p-4">
-                <div className="min-w-full">
-                  <div className="lg:grid lg:grid-cols-12 lg:auto-rows-min lg:gap-x-8">
-                    <div className="lg:col-span-12">
-                      <div className="flex gap-4 sm:items-center sm:justify-between sm:flex-row flex-col">
-                        <div className="flex flex-col gap-2">
-                          <h1 className="text-2xl font-medium text-gray-900">
-                            {stream?.publicTitle}
-                          </h1>
+    <StreamClaimingProvider chainId={chainId} contractAddress={contractAddress}>
+      {({ data: { stream } }) => (
+        <>
+          <main className="mx-auto max-w-lg flex items-center p-4">
+            <div className="min-w-full">
+              <div className="lg:grid lg:grid-cols-12 lg:auto-rows-min lg:gap-x-8">
+                <div className="lg:col-span-12">
+                  <h1 className="text-2xl font-medium text-gray-900">
+                    {stream?.publicTitle ||
+                      stream?.contractAddress?.substring(0, 8)}
+                  </h1>
+
+                  <div className="mt-6">
+                    <section className="mt-6 bg-gray-50 rounded-lg px-4 py-6 sm:p-6 lg:p-8 lg:mt-0 lg:col-span-5">
+                      <dl className={"space-y-4"}>
+                        <div className="flex items-center justify-between">
+                          <dt className="flex items-center text-sm text-gray-600">
+                            <span>Your NFTs</span>
+                          </dt>
+                          <dd className="text-sm font-medium text-gray-900">
+                            <StreamEligibleNfts />
+                          </dd>
                         </div>
-                        <div>
-                          <div className="text-md font-medium text-gray-900">
-                            <VestedHolderStreamRate />
+                        <div className="border-t border-gray-200 pt-4 flex items-center justify-between">
+                          <dt className="flex flex-col gap-1 text-sm text-gray-600">
+                            <span>Your total claimed</span>
+                            <small className="text-xs flex-shrink-0 text-gray-400">
+                              Sum of all previous claims
+                            </small>
+                          </dt>
+                          <dd className="text-sm font-medium text-gray-900">
+                            <StreamTotalClaimed />
+                          </dd>
+                        </div>
+                        <div className="border-t border-gray-200 pt-4 flex items-center justify-between">
+                          <dt className="flex flex-col gap-1 text-sm text-gray-600">
+                            <span>Emission rate</span>
+                            <small className="text-xs flex-shrink-0 text-gray-400">
+                              Amount of tokens released for all holders{" "}
+                            </small>
+                          </dt>
+                          <dd className="text-sm font-medium text-gray-900">
+                            <StreamEmissionRate />
+                          </dd>
+                        </div>
+                        <div className="border-t border-gray-200 pt-4 flex items-center justify-between">
+                          <dt className="flex flex-col gap-1 text-sm text-gray-600">
+                            <span>Claim window</span>
+                            <small className="text-xs flex-shrink-0 text-gray-400">
+                              How often can you claim
+                            </small>
+                          </dt>
+                          <dd className="text-sm font-medium text-gray-900">
+                            <StreamEmissionTimeUnit />
+                          </dd>
+                        </div>
+                        <div className="border-t border-gray-200 pt-4 flex items-center justify-between">
+                          <dt className="text-base font-medium text-gray-900">
+                            Claimable now for you
+                          </dt>
+                          <dd className="text-base font-medium text-gray-900">
+                            <StreamClaimableAmount />
+                          </dd>
+                        </div>
+                      </dl>
+
+                      <div className="mt-6 flex flex-col justify-center items-center">
+                        {/* Claim button */}
+                        <ConnectButton className={buttonClass}>
+                          <SwitchChainButton
+                            requiredChainId={Number(chainId)}
+                            className={buttonClass}
+                          >
+                            <StreamClaimButton className={buttonClass} />
+                          </SwitchChainButton>
+                          <div className="mt-4">
+                            <WalletDropdown />
                           </div>
-                        </div>
+                        </ConnectButton>
                       </div>
-
-                      <div className="mt-6">
-                        <section className="mt-6 bg-gray-50 rounded-lg px-4 py-6 sm:p-6 lg:p-8 lg:mt-0 lg:col-span-5">
-                          <dl className={"space-y-4"}>
-                            <div className="flex items-center justify-between">
-                              <dt className="flex items-center text-sm text-gray-600">
-                                <span>Your NFTs</span>
-                              </dt>
-                              <dd className="text-sm font-medium text-gray-900">
-                                <StreamEligibleNfts />
-                              </dd>
-                            </div>
-                            <div className="border-t border-gray-200 pt-4 flex items-center justify-between">
-                              <dt className="flex flex-col text-sm text-gray-600">
-                                <span>Your total claimed</span>
-                                <small className="flex-shrink-0 text-gray-400">
-                                  (all previous claims)
-                                </small>
-                              </dt>
-                              <dd className="text-sm font-medium text-gray-900">
-                                <StreamTotalClaimed />
-                              </dd>
-                            </div>
-
-                            <div className="border-t border-gray-200 pt-4 flex items-center justify-between">
-                              <dt className="flex flex-col text-sm text-gray-600">
-                                <span>Your released amount</span>
-                                <small className="flex-shrink-0 text-gray-400 hover:text-gray-500">
-                                  (claimable{" "}
-                                  <VestedHolderStreamTimeUnit className="inline" />
-                                  )
-                                </small>
-                              </dt>
-                              <dd className="text-sm font-medium text-gray-900">
-                                <VestedHolderStreamReleasedAmount />
-                              </dd>
-                            </div>
-                            <div className="border-t border-gray-200 pt-4 flex items-center justify-between">
-                              <dt className="text-base font-medium text-gray-900">
-                                Claimable now for you
-                              </dt>
-                              <dd className="text-base font-medium text-gray-900">
-                                <StreamClaimableAmount />
-                              </dd>
-                            </div>
-                          </dl>
-
-                          <div className="mt-6 flex flex-col justify-center items-center">
-                            {/* Claim button */}
-                            <ConnectButton className={buttonClass}>
-                              <SwitchChainButton
-                                requiredChainId={Number(STREAM_CHAIN_ID)}
-                                className={buttonClass}
-                              >
-                                <StreamClaimButton className={buttonClass} />
-                              </SwitchChainButton>
-
-                              <DisconnectButton className="text-indigo-700 text-sm mt-4" />
-                            </ConnectButton>
-                          </div>
-                        </section>
-                      </div>
-                    </div>
-
-                    <div className="mt-4 lg:col-span-12">
-                      <StreamStatusBar />
-
-                      <div className="mt-4">
-                        <h2 className="mt-4 text-sm font-medium text-gray-900">
-                          Description
-                        </h2>
-
-                        <div className="mt-3 prose prose-sm text-gray-400">
-                          Total claimed by all holders:{" "}
-                          <StreamTotalClaimed
-                            className="inline font-bold"
-                            calculationMode="OVERALL"
-                          />
-                        </div>
-                        <div className="mt-3 prose prose-sm text-gray-500">
-                          {stream?.publicDescription}
-                        </div>
-                      </div>
-                    </div>
+                    </section>
                   </div>
                 </div>
-              </main>
-            </>
-          )}
-        </VestedHolderStreamClaimingProvider>
-      </StreamClaimingProvider>
 
-      {/* OR, import the whole prebuilt section component:
-      
-        <VestedHolderStreamClaimingSection
-          chainId={Number(STREAM_CHAIN_ID)}
-          contractAddress={STREAM_CONTRACT_ADDRESS}
-        />
-      
-      */}
-    </div>
+                <div className="mt-4 lg:col-span-12">
+                  <StreamStatusBar />
+
+                  {stream?.publicDescription ? (
+                    <div className="mt-4">
+                      <h2 className="mt-4 text-sm font-medium text-gray-900">
+                        Description
+                      </h2>
+
+                      <div className="mt-3 prose prose-sm text-gray-500">
+                        {stream?.publicDescription}
+                      </div>
+                    </div>
+                  ) : null}
+                </div>
+              </div>
+            </div>
+          </main>
+        </>
+      )}
+    </StreamClaimingProvider>
   );
 }
 
